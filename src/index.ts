ec536a00c124ff116c049202287cc371db5335d0
ART1KZ <artemkiselev18072k6@mail.ru>
2024-12-08 17:49:02 +0500

    add authorization

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,64 @@
-import { Bot } from "grammy";
+import { Bot, InlineKeyboard } from "grammy";
+import { createClient } from '@supabase/supabase-js'
 import "dotenv/config"
 
-const BOT_TOKEN: string = process.env.BOT_TOKEN as string
+const BOT_TOKEN = process.env.BOT_TOKEN as string
+const bot = new Bot(BOT_TOKEN)
 
-const bot = new Bot(String(BOT_TOKEN))
+const supabaseUrl = process.env.SUPABASE_URL as string
+const supabaseToken = process.env.SUPABASE_TOKEN as string
+
+const supabase = createClient(supabaseUrl, supabaseToken)
+
+bot.command("start", async (ctx) => {
+    const userTelegramID = ctx.update?.message?.from.id
+
+    
+    try {
+        if (userTelegramID) {
+            let { data: users, error } = await supabase
+                .from('users')
+                .select('*')
+                .eq("telegram_id", userTelegramID)
+
+            if (error) {
+                ctx.reply("Возникла ошибка при регистрации. Попробуйте еще раз, для этого напишите /start");
+                console.error(error);
+
+                return;
+            }
+
+            // if user doesn't exist in the db adding new record on table
+            if (users === null || users.length === 0) {
+                const { data, error } = await supabase
+                    .from('users')
+                    .insert([
+                        { telegram_id: userTelegramID },
+                    ])
+
+                if (error) {
+                    ctx.reply("Возникла ошибка при регистрации. Попробуйте еще раз, для этого напишите /start");
+                    console.error(error);
+
+                    return;
+                }
+
+                ctx.reply("Добро пожаловать! Вы успешно авторизовались")
+
+            // if user already exists
+            } else {
+                ctx.reply("С возвращением!")
+            }
+        }
+    } catch (error) {
+        console.error("Произошла ошибка: " + error)
+    }
+
+});
 
 bot.on("message", (ctx) => {
-    console.log(ctx.update.message.text)
-    ctx.reply("Hi there!")
+    console.log(ctx.update.message)
+    ctx.reply("Привет!")
 });
 
 bot.start();
